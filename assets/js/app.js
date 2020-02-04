f3e1046d5939f46e8cab07c0219c390248467c00
stephane.ardin@gmail.com <stephane.ardin@gmail.com>
2020-02-04 10:33:28 +0100

    correction contactForm

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-let $ = require('jquery');
+const $ = require('jquery');
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
@@ -13,10 +13,12 @@ require('select2');
 
 let $contactButton = $('#contactButton');
 $contactButton.click(e => {
+    console.log('clic');
     e.preventDefault();
     $('#contactForm').slideDown();
     $contactButton.slideUp();
 })
+console.log('trop');
 
 $('select').select2();
 
