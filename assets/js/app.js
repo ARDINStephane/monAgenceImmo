ceb97ba3e9f89879879e4b14dc1fb28a878bcc7e
stephane.ardin@gmail.com <stephane.ardin@gmail.com>
2020-02-04 10:35:54 +0100

    nettoyage app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,5 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 const $ = require('jquery');
-// any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
-
 require('select2');
 
 let $contactButton = $('#contactButton');
@@ -18,11 +9,6 @@ $contactButton.click(e => {
     $('#contactForm').slideDown();
     $contactButton.slideUp();
 })
-console.log('trop');
 
 $('select').select2();
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
